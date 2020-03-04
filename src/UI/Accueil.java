866ce074a9bf02a9be823463eafbe6a6a327c3c6
Will <williamgarrofeb4@gmail.com>
2020-03-04 12:59:10 +0100

    Ajout UI

---
+++ src/UI/Accueil.java
@@ -0,0 +1,16 @@
+package UI;
+
+import javax.swing.*;
+
+public class Accueil {
+    private JTextField textFieldRecherche;
+    private JButton rechercheButton;
+    private JList ListeAnnonce;
+    private JButton connexionButton;
+    private JPanel panel;
+    private JButton button1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
---
+++ src/UI/Inscription.java
@@ -0,0 +1,14 @@
+package UI;
+
+import javax.swing.*;
+
+public class Inscription {
+    private JPanel panel1;
+    private JTextField Login;
+    private JTextField Password;
+    private JButton buttonValidation;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
