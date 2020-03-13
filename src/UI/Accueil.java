2d5eae809a96657128dd223e8e901eae738804aa
Will <williamgarrofeb4@gmail.com>
2020-03-13 08:45:30 +0100

    UI setup

--- src/UI/Accueil.java
+++ src/UI/Accueil.java
@@ -1,6 +1,10 @@
 package UI;
 
+import dao.JpaAnnonceDao;
+import metier.AnnonceEntity;
+
 import javax.swing.*;
+import java.util.Collection;
 
 public class Accueil {
     private JTextField textFieldRecherche;
@@ -9,14 +13,24 @@ public class Accueil {
     private JPanel panel;
     private JButton button1;
     private JList list1;
+    private JpaAnnonceDao JpaAnnconce;
+    private Collection<AnnonceEntity> Annonce;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
 
     }
 
+    public static void main(final String[] args) throws Exception {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new Accueil().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
     private void $$$setupUI$$$() {
         createUIComponents();
-
+        Annonce.addAll(JpaAnnconce.findAll());
     }
 }
